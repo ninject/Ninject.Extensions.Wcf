0da9ffa6d58eca0565ae5e32cd6630b79eedb002
Chris Hafey <chafey@gmail.com>
2011-12-17 15:23:53 -0600

    Added example for using Ninject with WCF REST Service Template 40(CS)

---
+++ src/WcfRestService/README.txt
@@ -0,0 +1,22 @@
+﻿This is an example project of using Ninject from a WCF REST Web Service based on the
+"WCF REST Service Template 40(CS)" found here:
+http://visualstudiogallery.msdn.microsoft.com/fbc7e5c1-a0d2-41bd-9d7b-e54c845394cd?SRC=VSIDE
+
+NOTE - This example is based on the version of Ninject that is currently under development.  It will not work 
+with the stable 2.2 version without changes.  Until 2.4 is released, you will need to get compatible versions of 
+each Ninject library from the teamcity site (http://teamcity.codebetter.com/project.html?projectId=project3&tab=projectOverview)
+or build from source.
+
+Steps followed:
+
+1) Create new project using the WCF REST Service Template 40(CS)
+2) Add dependencies on Ninject.Extensions.Wcf, Ninject and Ninject.Web.Common.  
+3) Delete all methods from Service1.cs except for GetCollection() (we only need to implement one for this example)
+4) Add interface IRepository.cs and add the method GetCollection()
+5) Create class Repository.cs that inherits from IRepository and and move code from Service1.GetCollection() to it
+6) Add ServiceModule.cs and inherit from NinjectModule, Bind IRepository to Repository in the overriden Load method
+7) Modify Global.asax.cs to inherit from NinjectHttpApplication.
+  8) Remove Application_Started method since the startup logic is managed by NinjectHttpApplication
+  9) Implement CreateKernel method to returnt he kernel with your bindings (via ServiceModule)
+  10) Call RegisterRoutes from OnApplicationStarted()
+
---
+++ src/WcfRestService/Service1.cs
@@ -0,0 +1,54 @@
+﻿//-------------------------------------------------------------------------------
+// <copyright file="Service1.cs" company="Ninject Project Contributors">
+//   Copyright (c) 2009-2011 Ninject Project Contributors
+//   Author: Chris Hafey (chafey@gmail.com)
+//
+//   Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+//   you may not use this file except in compliance with one of the Licenses.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//   or
+//       http://www.microsoft.com/opensource/licenses.mspx
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+//-------------------------------------------------------------------------------
+
+namespace WcfRestService
+{
+	using System.Collections.Generic;
+	using System.ServiceModel;
+	using System.ServiceModel.Activation;
+	using System.ServiceModel.Web;
+
+	// Start the service and browse to http://<machine_name>:<port>/Service1/help to view the service's generated help page
+	// NOTE: By default, a new instance of the service is created for each call; change the InstanceContextMode to Single if you want
+	// a single instance of the service to process all calls.	
+	[ServiceContract]
+	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+	[ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+	// NOTE: If the service is renamed, remember to update the global.asax.cs file
+	public class Service1
+	{
+
+		private readonly IRepository _repository;
+
+		public Service1(IRepository repository)
+		{
+			_repository = repository;
+		}
+
+		[WebGet(UriTemplate = "")]
+		public List<SampleItem> GetCollection()
+		{
+			// TODO: Replace the current implementation to return a collection of SampleItem instances
+			return _repository.GetCollection();
+		}
+
+	}
+}
