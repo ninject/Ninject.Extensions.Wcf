427435a6a81ff6ccec4c50ac74fb420ec0f06c01
mortenn <morten@runsafe.no>
2011-12-21 14:16:59 +0100

    Sample for using Ninject with fileless activation of a WCF service library

---
+++ src/Examples/WcfServiceLibrary/ServiceLayer/TimeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WcfServiceLibrary.Contracts;
+
+namespace WcfServiceLibrary.ServiceLayer
+{
+	public class TimeService : ITimeService
+	{
+		public TimeService(ISystemClock systemClock)
+		{
+			clock = systemClock;
+		}
+
+		public DateTime WhatTimeIsIt()
+		{
+			return clock.Time;
+		}
+
+		private readonly ISystemClock clock;
+	}
+}
